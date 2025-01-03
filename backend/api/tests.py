a84c8234ff5360f9881dea4eb35198d83f231f1b
Andy R. Terrel <andy.terrel@gmail.com>
2025-01-03 07:01:46 -0600

    Adds test data and testing for api app.

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Event, Recipe
+
+class EventTestCase(TestCase):
+    fixtures = ['event_data.json']
+
+    def test_event(self):
+        event = Event.objects.get(title='Soccer Practice')
+        self.assertEqual(event.assigned_to, 'Mom')
+
+class RecipeTestCase(TestCase):
+    fixtures = ['recipe_data.json']
+
+    def test_recipe(self):
+        recipe = Recipe.objects.get(name='Spaghetti')
+        self.assertIsNotNone(recipe.ingredients)
+
